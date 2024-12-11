d120d3495f8db66f8ead0553b1df8ea59c945f42
pablo galvan <gvpablodaniel@gmail.com>
2024-12-10 22:16:46 -0300

    set theme and color

--- app/src/main/java/com/pdgalvan/cryptotracker/ui/theme/Type.kt
+++ app/src/main/java/com/pdgalvan/cryptotracker/ui/theme/Type.kt
@@ -1,34 +1,44 @@
 package com.pdgalvan.cryptotracker.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.googlefonts.Font
+import com.pdgalvan.cryptotracker.R
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Roboto")
+
+val fontFamily = FontFamily(
+    Font(
+        googleFont = fontName,
+        fontProvider = provider,
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+// Default Material 3 typography values
+val baseline = Typography()
+
+val AppTypography = Typography(
+    displayLarge = baseline.displayLarge.copy(fontFamily = fontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = fontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = fontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = fontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = fontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = fontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = fontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = fontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = fontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = fontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = fontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = fontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = fontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = fontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = fontFamily),
 )
